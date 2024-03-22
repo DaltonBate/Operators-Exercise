14aaa906d8e900394b795284e885df63e52fa753
Dalton Bate <dilton8763@gmail.com>
2024-03-22 18:10:20 -0400

    Completed C# operators

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,32 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //For division, create two integer variables called a and b.
+
+            var a = 17;
+            var b = 4;
+            var quotient = a / b; 
+            var remainder = a % b;
+
+            Console.WriteLine($"{a} {b} {quotient} remainder {remainder}");
+
+            Console.WriteLine("Please enter the radius of your circle" );  
+            var userInput = Console.ReadLine();
+
+            var radius = double.Parse(userInput);
+
+           AreaOfCircle(radius);
+
+            AreaOfCircle(15);
+        }
+        
+        //define a merthod - which performs some functionality. that can be used repeatedly
+        public static double AreaOfCircle(double radius)
+        {
+            //this is the scope of the method
+            var area = Math.PI * Math.Pow(radius, 2);
+           
+            return area;
         }
     }
 }
